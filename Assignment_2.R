32c2b82a98d35af70e4b3e51e6f89c26e2c3d6af
ChiaraSauren <chiara.sauren@outlook.de>
2022-07-03 16:28:07 +0200

    a) korrigiert und funktioniert

--- Assignment_2.R
+++ Assignment_2.R
@@ -1,27 +1,28 @@
 library(stats)
+library(profvis)
 ## 1
-
-neg_likelihood_NB <- function(p) -prod(dnbinom(x,size=10,prob = p))
-neg_loglikelihood_NB <- function(p) -sum(log(dnbinom(x,size=10,prob = p)))
-
+x <- (5)
 
 lprob_nbinomial <- function(p){
-  likelihood_NB_optim = optim(par = 0.5,
-                              fn = neg_likelihood_NB,
-                              lower = 1e-8,
-                              upper = 1-1e-8,
-                              method = 'L-BFGS-B');
-  loglikelihood_NB_optim = optim(par = 0.5,
-                                 fn=neg_loglikelihood_NB,
-                                 lower = 1e-8,
-                                 upper = 1-1e-8,
-                                 method = 'L-BFGS-B');
+  #negative likelihood
+  neg_likelihood_NB <- list(-prod(dnbinom(x,size=10,prob = p)),
+  #negative log-likelihood
+  neg_loglikelihood_NB <- (-sum(log(dnbinom(x,size=10,prob = p))))
+                            )
+  return(neg_likelihood_NB)
+  
  
- 
+
 }
+
 
 lprob_nbinomial(0.2)
 
-likelihood_NB_optim$par
 
+# generating the dnbinom call should be the most computationally demanding
+
+#b
+
+# ephermal environments are meant to last for  only a short amount of time. Thus they
+# just are just temporarily generated to serve a function in e.g only a single call or R function.
 
