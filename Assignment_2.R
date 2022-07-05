1de7f1055f870cb5717055c3306a0fd2434abceb
ChiaraSauren <chiara.sauren@outlook.de>
2022-07-05 18:19:47 +0200

    Richtig a) und b)

--- Assignment_2.R
+++ Assignment_2.R
@@ -2,28 +2,51 @@ library(stats)
 library(profvis)
 library(purrr)
 ## 1
-x <- (5)
 
-lprob_nbinomial <- function(p){
-  #negative likelihood
-  neg_likelihood_NB <- list(-prod(dnbinom(x,size=10,prob = p)),
-  #negative log-likelihood
-  neg_loglikelihood_NB <<- (-sum(log(dnbinom(x,size=10,prob = p))))
-  )
-  return(neg_likelihood_NB)
-                          
+
+# function to calculate log-likelihood function with k = 10
+
+lprob_nbinomial <- function(pi, x){
+  n <- length(x)
+  (-sum((log(choose(x+10-1, x)))+ 10 * log(pi)+x * log(1-pi)))
 }
+lprob_nbinomial(0.5, x)                          
 
 
-lprob_nbinomial(0.2)
+#generate some data to test the function:
+x <- rnbinom( n = 1e5, size = 5, prob = .2)
+x <- rnbinom( n = 1e6, size = 5, prob = .2)
+length(x)
+
+lprob_nbinomial(0.5)  # test function
+
+# profile collecting of example data and parts of the function in comparison
+profvis({
+
+    n <- length(x)
+  test <-  (-sum((log(choose(x+10-1, x)))+ 10 * log(0.5)+x * log(1-0.5)))
+  
+})
+
+# with increasing length of the example data x, the computation time increases
+#accordingly. With n = 100000 (1e5) observations the lprob_nbinomial function takes up
+# to 10 ms, using up 2.3 mbit of memory space. With n = 1000000 (1e6) the computation 
+# time already takes 110 ms and takes up to 22.9 mbits of memory space.
 
 
-# generating the dnbinom call should be the most computationally demanding
 
 #b )
 
-# ephermal environments are meant to last for  only a short amount of time. Thus they
-# just are just temporarily generated to serve a function in e.g only a single call or R function.
+# ephemeral environments are meant to serve as a testing, temporary or dynamic environment
+# that show a replica of the actual work-environment. They usually use replicated data and
+# test whether a test or step is applicable before risking on damaging the real work environmen.
+# They help in accelerating the programming or software development cycle, limiting the need to
+# rework broad parts of the written code.
+# In comparison to a ephemeral environment, the function factory 
+# encloses the environment of the manufactured function. This serves as the execution
+# environment of the function factory.
+
+
 
 
 #c)
